2fef71b075e729abad8cd25db793446f99cc3556
brayanrodallega <thebrayan1a1@outlook.com>
2023-02-28 12:39:17 -0500

    Fetch POST

---
+++ src/promise/challenge-post.js
@@ -0,0 +1,30 @@
+import fetch  from "node-fetch";
+const API = 'https://api.escuelajs.co/api/v1';
+
+function postData(urlApi, data) {
+
+    const response = fetch(urlApi, {
+        method: 'POST',
+        mode: 'cors',
+        creedentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    return response;
+}
+
+const data = {
+    "title": "230",
+    "price": 999,
+    "description": "A description",
+    "categoryId": 1,
+    "images": ["https://placeimg.com/640/480/any"]
+}
+
+postData(`${API}/products`, data)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
